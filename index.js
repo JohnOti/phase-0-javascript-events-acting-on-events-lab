be89d60c1b546bde9ac3b992d3e30b20f5bf2558
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-04 21:16:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger1 = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger1.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger1.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const rightNumbers = dodger1.style.left.replace("px", "");
+  const left = parseInt(rightNumbers, 10);
+
+  if (left > 0) {
+    dodger1.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
